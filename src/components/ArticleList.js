a675f534bf5ed9b0a5703c9171a94143433bf024
joannayhs <joannayuhas@gmail.com>
2022-02-11 16:03:55 -0500

    added article component

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Article from './Article'
 
 function ArticleList({posts}){
     return(
         <main>
             {posts.map( post => {
-                return <Article post={post}/>
+                return <Article key={post.id} title={post.title} date={post.date} preview={post.preview}/>
             })}
         </main>
     )
