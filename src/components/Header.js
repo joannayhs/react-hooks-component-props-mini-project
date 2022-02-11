a1831c6587f3197605a88fc4390d8f1d5c6d1f50
joannayhs <joannayuhas@gmail.com>
2022-02-11 15:48:35 -0500

    built header component and added it to app

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
-function Header(){
+function Header({ name }){
     return (
         <>
-        
+        <h1>{name}</h1>
         </>
     )
 }
+
+export default Header
