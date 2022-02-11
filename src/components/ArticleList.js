1a07c6dcc715746ddacb65863ba45a8cdc107899
joannayhs <joannayuhas@gmail.com>
2022-02-11 16:13:58 -0500

    made changes to attempt to get tests passing

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -2,10 +2,11 @@ import React from 'react'
 import Article from './Article'
 
 function ArticleList({posts}){
+    
     return(
         <main>
-            {posts.map( post => {
-                return <Article key={post.id} title={post.title} date={post.date} preview={post.preview}/>
+            {posts.map(post => {
+                return <Article key={post.id} title={post.title} date={post.date} preview={post.preview} />
             })}
         </main>
     )
