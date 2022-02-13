698f349026c5d9e71b766064972c362bb53aa583
joannayhs <joannayuhas@gmail.com>
2022-02-12 20:31:05 -0500

    added emojis for bonus

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,10 +1,30 @@
 import React from 'react'
 
-function Article({title, date="January 1, 1970", preview}) {
+function divideMinutes(minutes) {
+    const coffeeCup = "☕️"
+    const bentoBox = "🍱"
+
+    let emojis = ""
+
+    if(minutes < 30){
+        for ( let i = 0; i < minutes; i += 5){
+            emojis += coffeeCup
+        }
+    }else if(minutes > 30){
+        for (let i = 0; i < minutes; i += 10){
+            emojis += bentoBox
+        }
+    }
+    return emojis
+}
+
+
+function Article({title, date="January 1, 1970", preview, minutes}) {
+
     return (
         <article>
             <h3>{title}</h3>
-            <small>{date}</small>
+            <small>{date}</small> <small>{divideMinutes(minutes) + minutes + " min read"}</small>
             <p>{preview}</p>
         </article>
     )
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -6,7 +6,7 @@ function ArticleList({posts}){
     return(
         <main>
             {posts.map(post => {
-                return <Article key={post.id} title={post.title} date={post.date} preview={post.preview} />
+                return <Article key={post.id} title={post.title} date={post.date} preview={post.preview} minutes={post.minutes}/>
             })}
         </main>
     )
